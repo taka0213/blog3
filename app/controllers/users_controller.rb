bfe642983a2c2e1272aa44e130c2a890bbc9c5b6
taka0213 <snowfaceweapon@gmail.com>
2019-04-19 20:14:20 +0900

    Delete

--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @nickname = current_user.nickname
-    @blogs = current_user.blogs.page(params[:page]).per(5).order("created_at DESC")
-  end
-end
